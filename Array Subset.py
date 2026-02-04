d7943d4d042020b32a678864c394aaa212619c9a
Adugna Tolessa <adugnatolessa5@gmail.com>
2026-02-04 15:25:59 +0300

    Refactor isSubset method using Counter for efficiency

--- Array Subset.py
+++ Array Subset.py
@@ -1,20 +1,9 @@
+from collections import Counter
 class Solution:
     def isSubset(self, a, b):
-        # for i in range(len(b)):
-        #     if b[i] not in a or len(b) > len(a):
-        #         return False
-        # return True
-        # # return b.issubset(a)
-    
-    
-        m, n = len(a), len(b)
-        for i in range(n):
-            found = False
-            for j in range(m):
-                if b[i] == a[j]:
-                    found = True
-                    a[j] = -1
-                    break
-            if not found:
+        x = Counter(a)
+        y = Counter(b)
+        for i in y:
+            if y[i] > x[i]:
                 return False
         return True
